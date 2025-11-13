03d7f6e13eebc46911eb796ab1c4ac1e951e162d
Shivam Sharma <shivanshsharma8899@gmail.com>
2025-11-13 09:02:52 +0530

    codebase

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,60 @@
+import HeroImg from "@/assets/images/hero.jpg";
+import OlovaLogo from "@/assets/images/olova.png";
+
+export default function About() {
+  return (
+    <>
+      <section id="about" className="py-16 md:py-32  text-white bg-[#04081A]">
+        <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
+          <h2 className="relative z-10 max-w-xl text-4xl font-medium lg:text-5xl text-white">
+            Engineer, Developer, Teacher, Innovator
+          </h2>
+          <div className="grid gap-6 sm:grid-cols-2 md:gap-12 lg:gap-24">
+            <div className="relative mb-6 sm:mb-0">
+              <div className="bg-linear-to-b aspect-76/59 relative rounded-2xl p-px from-zinc-300 to-transparent">
+                <img
+                  src={HeroImg}
+                  className="rounded-[15px] shadow block"
+                  alt="payments illustration"
+                  width={1207}
+                  height={929}
+                />
+              </div>
+            </div>
+
+            <div className="relative space-y-4">
+              <p className="text-white">
+                Hello! I'm Shivam Sharma, a passionate developer specializing in building dynamic websites and intelligent ML models.
+.{" "}
+                <span className="font-bold text-white">
+                  I’m dedicated to blending creativity and logic — building sleek websites and powerful ML models
+                </span>
+                , that solve real-world problems.
+              </p>
+              <p className="text-white">
+                My focus is on making web development faster, easier, and
+                accessible to all developers. Currently, I'm expanding into
+                backend development to grow as a full-stack developer and create
+                seamless, robust web applications.
+              </p>
+
+              <div className="pt-6">
+                <blockquote className="border-l-4 border-gray-300 pl-4">
+                  <p className="text-white">
+                    I build. I learn. I evolve. From websites to machine learning models, I create systems that perform, adapt, and make a real impact.
+                  </p>
+
+                  <div className="mt-6 space-y-3">
+                    <cite className="block font-medium text-white">
+                      Shivam Sharma, Artistic Engineer
+                    </cite>
+                  </div>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
